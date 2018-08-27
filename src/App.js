9f204b0554f3806e1d62a8e07aada939a3c2d9ae
alekz7 <ing.aragon.ipn@gmail.com>
2018-08-27 10:57:13 -0500

    trabajando en el boton search

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import foods from './data/foods';
+import FoodBox from './components/FoodBox';
+import { Input } from 'antd';
+
+const Search = Input.Search;
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: foods,
+    };
+  }
+  
+  handleChange(event){    
+    console.log(event.target.value);
+    console.log(foods);
+    var filterFoods = foods.filter(function (obj) {
+      return obj.name.toLowerCase().includes(event.target.value.toLowerCase());
+    });    
+    
+    console.log(filterFoods);
+
+    this.setState({
+      data: filterFoods,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Lab IronNutritions</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+            <Search placeholder="input search text"
+              onChange={this.handleChange}
+              onSearch={value => console.log(value)}
+              enterButton
+            />
+          </div>
+        </div>
+
+        <div className="columns">
+          <div className="column is-half is-offset-one-quarter">
+
+            {foods.map((food, index) =>{ return <FoodBox {... food} index={index}/> })}
+
+          </div>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './App.css';
+import 'bulma/css/bulma.css';
+import 'antd/dist/antd.css';
+// import './css/style.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
